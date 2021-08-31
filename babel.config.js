8e1857550635689d944b28f9e4c5cf584f75fe68
Mehul2410 <mehulgawhale18hc@student.mes.ac.in>
2021-08-31 13:37:09 +0530

    full react native setup

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
 module.exports = function (api) {
     api.cache(true);
     return {
-        presets: ["babel-preset-expo"]
+        presets: ["babel-preset-expo"],
+        plugins: [
+            [
+                "module-resolver",
+                {
+                    alias: {
+                        "@screens": "./src/screens",
+                        "@config": "./src/config",
+                        "@contexts": "./src/contexts",
+                        "@components": "./src/components",
+                        "@utils": "./src/utils",
+                        "@assest": "./assets"
+                    }
+                }
+            ]
+        ]
     };
 };
